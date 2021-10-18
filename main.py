3a0bbffd832b82d66f0da8688074e1b1c9a65d63
meliodas0n <mrunalnarayana@gmail.com>
2021-10-18 15:48:57 +0530

    cargame for fun

---
+++ main.py
@@ -0,0 +1,106 @@
+import pygame
+import time
+import math
+from utils import scale_image, blit_rotate_center
+
+GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
+TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
+
+TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
+FINISH = pygame.image.load("imgs/finish.png")
+
+RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
+GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"), 0.55)
+
+WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Racing GAME")
+
+FPS = 144
+
+def draw(win, images, player_car):
+    for img, pos in images:
+        win.blit(img, pos)
+    player_car.draw(win)
+    pygame.display.update()
+
+def move_player(player_car):
+    keys = pygame.key.get_pressed()
+    moved = False
+
+    if keys[pygame.K_a]:
+        player_car.rotate(left = True)
+    if keys[pygame.K_d]:
+        player_car.rotate(right = True)
+    if keys[pygame.K_w]:
+        moved = True
+        player_car.move_forward()
+    if keys[pygame.K_s]:
+        moved = True
+        player_car.move_backward()
+
+    if not moved:
+        player_car.reduce_speed()
+
+class Car:
+    IMG = RED_CAR
+    
+    def __init__(self, max_vel, rotation_vel) -> None:
+        self.img = self.IMG
+        self.max_vel = max_vel
+        self.vel = 0
+        self.rotation_vel = rotation_vel
+        self.angle = 0
+        self.x, self.y = self.START_POS
+        self.acceleration = 0.1
+
+    def rotate(self, left = False, right = False) -> None:
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+
+    def draw(self, win) -> None:
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+
+    def move_forward(self) -> None:
+        self.vel = min(self.vel + self.acceleration, self.max_vel)
+        self.move()
+
+    def move_backward(self) -> None:
+        self.vel = min(self.vel - self.acceleration, -self.max_vel / 2)
+        self.move()
+
+    def move(self) -> None:
+        radians = math.radians(self.angle)
+        vertical = math.cos(radians) * self.vel
+        horizontal = math.sin(radians) * self.vel
+
+        self.y -= vertical
+        self.x -= horizontal
+
+class PlayerCar(Car):
+    IMG = RED_CAR
+    START_POS = (180, 200)
+
+    def reduce_speed(self) -> None:
+        self.vel = max(self.vel - self.acceleration / 2, 0)
+        self.move()
+
+run = True
+clock = pygame.time.Clock()
+images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
+player_car = PlayerCar(4, 4)
+
+while run:
+    clock.tick(FPS)
+    draw(WIN, images, player_car)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+    
+    move_player(player_car)
+
+pygame.quit()
